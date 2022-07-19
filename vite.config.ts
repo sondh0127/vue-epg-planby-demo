118b24bc443e3e284ffa5403207b19f2cf9824f6
sondh0127 <sondh0127@gmail.com>
2022-07-19 08:24:54 +0700

    4 files modified, 2 added, 1 deleted

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Unocss from 'unocss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: './src/auto-imports.d.ts',
+      dirs: ['./src/composables'],
+      vueTemplate: true,
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: './src/components.d.ts',
+    }),
+    Unocss(),
+  ],
+});
