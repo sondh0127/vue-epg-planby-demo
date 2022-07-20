0ee5886114143aa8011c6123a248e432f117ece3
sondh0127 <sondh0127@gmail.com>
2022-07-20 20:00:15 +0700

    6 files modified

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,8 +9,14 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
+    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElSwitch: typeof import('element-plus/es')['ElSwitch']
+    ElTimePicker: typeof import('element-plus/es')['ElTimePicker']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
   }
 }
--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -6,5 +6,22 @@ import type { Channel, Program } from '@sondh0127/vue-epg-planby';
 export const fetchChannels = async () =>
   new Promise<Channel[]>((res) => setTimeout(() => res(channels), 400));
 
-export const fetchEpg = async () =>
-  new Promise<Program[]>((res) => setTimeout(() => res(epg), 500));
+export const fetchEpg = async (date: string, date2: string) => {
+  return new Promise<Program[]>((res) =>
+    setTimeout(
+      () =>
+        res(
+          epg.map((item) => ({
+            ...item,
+            since: item.since
+              .replace('2022-07-17', date)
+              .replace('2022-07-18', date2),
+            till: item.till
+              .replace('2022-07-17', date)
+              .replace('2022-07-18', date2),
+          }))
+        ),
+      500
+    )
+  );
+};
